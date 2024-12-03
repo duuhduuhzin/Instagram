83a3909828f7db1d0b047510ce12702494c769ee
duuhduuhzin <ebcollela@gmail.com>
2024-12-03 13:59:33 -0300

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,53 @@
+var crl1 = document.getElementById('myCrl1').getContext('2d');
+
+crl1.beginPath();
+crl1.arc(60,100,40,0,2 *Math.PI);
+crl1.fill();
+crl1.linewidth = 5;
+crl1.strokeStyle = "#dbdbdb";
+crl1.stroke();
+
+var crl2 = document.getElementById('myCrl2').getContext('2d');
+
+crl2.beginPath();
+crl2.arc(60,100,40,0,2 *Math.PI);
+crl2.fill();
+crl2.linewidth = 5;
+crl2.strokeStyle = "#dbdbdb";
+crl2.stroke();
+
+var crl3 = document.getElementById('myCrl3').getContext('2d');
+
+crl3.beginPath();
+crl3.arc(60,100,40,0,2 *Math.PI);
+crl3.fill();
+crl3.linewidth = 5;
+crl3.strokeStyle = "#dbdbdb";
+crl3.stroke();
+
+var crl4 = document.getElementById('myCrl4').getContext('2d');
+
+crl4.beginPath();
+crl4.arc(60,100,40,0,2 *Math.PI);
+crl4.fill();
+crl4.linewidth = 5;
+crl4.strokeStyle = "#dbdbdb";
+crl4.stroke();
+
+var crl5 = document.getElementById('myCrl5').getContext('2d');
+
+crl5.beginPath();
+crl5.arc(60,100,40,0,2 *Math.PI);
+crl5.fill();
+crl5.linewidth = 5;
+crl5.strokeStyle = "#dbdbdb";
+crl5.stroke();
+
+var crl6 = document.getElementById('myCrl6').getContext('2d');
+
+crl6.beginPath();
+crl6.arc(60,100,40,0,2 *Math.PI);
+crl6.fill();
+crl6.linewidth = 5;
+crl6.strokeStyle = "#dbdbdb";
+crl6.stroke();
